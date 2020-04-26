c00f097cc6e46551035dd1132223c9bb7e0591ba
89apt89 <89apt89@protonmail.ch>
2020-04-25 22:13:01 -0400

    Fix Blocks#isEmpty erroring on <1.12 versions.

--- src/main/java/systems/kscott/randomspawnplus3/util/Blocks.java
+++ src/main/java/systems/kscott/randomspawnplus3/util/Blocks.java
@@ -5,7 +5,7 @@ import org.bukkit.block.Block;
 public class Blocks {
 
     public static boolean isEmpty(Block block) {
-        if (XMaterial.isOneEight()) {
+        if (XMaterial.getVersion() <= 12) {
             return (block.getType() == XMaterial.AIR.parseMaterial() || block.getType() == XMaterial.VOID_AIR.parseMaterial() || block.getType() == XMaterial.CAVE_AIR.parseMaterial());
         }
         return (block.isEmpty() || block.getType().isAir());
