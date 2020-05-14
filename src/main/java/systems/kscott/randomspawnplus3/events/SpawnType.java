e6db2974ccc9cc963ce57e77c2c5aa85b18fe61b
89apt89 <89apt89@protonmail.ch>
2020-05-14 14:51:42 -0400

    Remove everything

--- src/main/java/systems/kscott/randomspawnplus3/events/SpawnType.java
+++
@@ -1,7 +0,0 @@
-package systems.kscott.randomspawnplus3.events;
-
-public enum SpawnType {
-    FIRST_JOIN,
-    ON_DEATH,
-    WILD_COMMAND
-}
--- src/main/java/systems/kscott/randomspawnplus3/exceptions/FinderTimedOutException.java
+++
@@ -1,4 +0,0 @@
-package systems.kscott.randomspawnplus3.exceptions;
-
-public class FinderTimedOutException extends Exception {
-}
