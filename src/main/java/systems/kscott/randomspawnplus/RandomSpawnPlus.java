b587278ad2c128fbd507c82d027a32fed4264a6c
89apt89 <89apt89@protonmail.ch>
2020-05-14 15:32:03 -0400

    Initialize Cacher and Finder

--- src/main/java/systems/kscott/randomspawnplus/RandomSpawnPlus.java
+++ src/main/java/systems/kscott/randomspawnplus/RandomSpawnPlus.java
@@ -1,6 +1,15 @@
 package systems.kscott.randomspawnplus;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import systems.kscott.randomspawnplus.spawn.Cacher;
+import systems.kscott.randomspawnplus.spawn.Finder;
 
 public class RandomSpawnPlus extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        Cacher.initialize();
+        Finder.initialize();
+    }
+
 }
